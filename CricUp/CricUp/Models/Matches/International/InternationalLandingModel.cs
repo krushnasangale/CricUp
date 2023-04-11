ba68e8abfc5005f5ed11645d2b25e77df8b2ced8
Krishna Sangale <krishna.sangale@waiin.com>
2023-04-11 17:15:49 +0530

    Added local dto

---
+++ CricUp/CricUp/Models/Matches/International/InternationalLandingModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CricUp.Models.Matches.International
+{
+    public class InternationalLandingModel
+    {
+        public int seriesId { get; set; }
+        public string seriesName { get; set; }
+    }
+}
